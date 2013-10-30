be8d7a1aa1c8953d6a2dd9aca670090f100b3e8a
Ralph Reid <beresfordjunior@me.com>
2013-10-30 10:06:59 +0000

    release course index

---
+++ ga_schedule_planner_app/app/controllers/courses_controller.rb
@@ -0,0 +1,30 @@
+class CoursesController < ApplicationController
+  def show
+    
+  end
+
+  def index 
+    @courses = Course.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+end
--- ga_schedule_planner_app/config/routes.rb
+++ ga_schedule_planner_app/config/routes.rb
@@ -1,4 +1,11 @@
 GaSchedulePlannerApp::Application.routes.draw do
+  
+  root to: 'courses#index'
+  resources :courses
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
