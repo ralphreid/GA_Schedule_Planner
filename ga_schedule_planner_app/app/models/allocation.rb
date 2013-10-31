d55c429d2790f178f81edf463022786557d57abe
Ralph Reid <beresfordjunior@me.com>
2013-10-31 12:05:26 +0000

    fixed foriegn key relationship on the allocations and enrollment models

--- ga_schedule_planner_app/app/models/allocation.rb
+++ ga_schedule_planner_app/app/models/allocation.rb
@@ -2,7 +2,7 @@ class Allocation < ActiveRecord::Base
   
   attr_accessible :instructor_id, :lesson_id
 
-  belongs_to :instructor
+  belongs_to :instructor, class_name: 'User'
   belongs_to :lesson
 
 
--- ga_schedule_planner_app/app/models/enrollment.rb
+++ ga_schedule_planner_app/app/models/enrollment.rb
@@ -2,7 +2,7 @@ class Enrollment < ActiveRecord::Base
   
   attr_accessible :student_id, :lesson_id, :enrollment_date, :payment_status
 
-  belongs_to :student
+  belongs_to :student, class_name: 'User'
   belongs_to :lesson
 
 
